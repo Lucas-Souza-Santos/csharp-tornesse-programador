b384df68fe4f1a5d8a5a42c54812505d0ca95d49
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-01-11 14:45:03 -0300

    Primeira Aula

---
+++ Tipos_Variaveis/Aula002.cs
@@ -0,0 +1,33 @@
+using System;
+
+class Aula002
+{
+    static void Main()
+    {
+        int i = 1;
+        var x = 10;
+        /*
+            Tipos de variáveis e conversões 
+            - String (conjunto de chars)
+            - int (4 bytes)
+            - Float (4 bytes)
+            - double (8 bytes)
+            - boolean (1 bytes)
+            - Decimal (1gbytes)
+            - char (2 bytes)
+            - long (8 bytes)
+            - Byte (1 byte) 
+
+            * uint   (4 B)
+            * sbyte  (1 B)
+            * short  (2 B)
+            * ushort (2 B)
+            * uLong  (8 B)
+
+            -> Conversões 
+            convert.ToString()
+            convert.ToInt()
+            convert.ToFloat()
+        */
+    }
+}

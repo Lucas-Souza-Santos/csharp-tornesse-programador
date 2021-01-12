827902dba14c2cc57c4a055699804411b00af2cf
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-01-12 12:19:23 -0300

    salvando arquivos

---
+++ Funcoes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Funcoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Sintaxe de função
+                Local   Tipo    Retorno     Nome    (Parâmetros)
+                Local: 
+                    - Public
+                    - Private 
+                    - Internal
+                    - Protected 
+                Tipo: 
+                    - Static 
+                    - Não Static
+                Retorno:
+                    - void
+                    - int 
+                    - string
+                    - double
+                    - bool
+            */
+            
+        }
+    }
+}
